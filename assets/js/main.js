a904ab252dda9c6731033e74a1297f4ca0774fde
Brian Hurst <brian.hurst@gsa.gov>
2017-12-08 14:14:27 -0500

    Allow AnchorJS targets to be defined in config

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,14 @@
 ---
 ---
 
-{% if site.anchor_js %}
-  // Adding anchor links to headers on docs layouts only
-  anchors.add('.usa-layout-docs h1, .usa-layout-docs h2, .usa-layout-docs h3');
-{% endif %}
+{%- if site.anchor_js_targets -%}
+  {%- assign _anchor_js_targets = site.anchor_js_targets -%}
+  {%- assign _prepended_targets = '' | split: '' -%}
+  {%- for _target in _anchor_js_targets -%}
+    {%- assign _prepended_target = _target | prepend: '.usa-layout-docs ' -%}
+    {%- assign _prepended_targets = _prepended_targets | push: _prepended_target -%}
+  {%- endfor -%}
+
+// Adding anchor links to headers on docs layouts only
+anchors.add('{{ _prepended_targets | join: ', ' }}');
+{%- endif -%}
